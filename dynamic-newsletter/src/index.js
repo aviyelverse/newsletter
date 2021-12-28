e60e1265b5a50fcfd619c17bb2f2b9b338c4537c
pramit-marattha <pramitmarattha5497@gmail.com>
2021-12-28 20:44:55 +0545

    upto read title made dynamic

--- dynamic-newsletter/src/index.js
+++ dynamic-newsletter/src/index.js
@@ -11,6 +11,11 @@ app.set("view engine", "hbs");
 app.get("", (req, res) => {
   res.render("index.hbs", {
     aviyelNewsletterTitle: "Aviyel Newsletter",
+    issueAndDate: "Issue 3 | 06/01/2022",
+    gardeningBigImage:
+      "https://user-images.githubusercontent.com/37651620/147542872-e3499b05-c33a-484c-952f-c850b361f05f.png",
+    readTitle: "READ",
+    
   });
 });
 
